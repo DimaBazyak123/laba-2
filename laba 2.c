3ae2ac5914419b5e0c9b5b2dfa85741c6557e46c
DimaBazyak123 <138712839+DimaBazyak123@users.noreply.github.com>
2023-07-05 21:47:34 +0300

    laba 2

---
+++ laba 2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int numbers[20];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int result = numbers[0];
+    for (int i = 1; i < n; i++) {
+        result = lcm(result, numbers[i]);
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+} 
